7eee0a8556b18ee0b1dff049122c885fe28d1eef
c-dez <c-dez@proton.me>
2026-08-29 00:50:38 -0600

    varios ajustes a my_car para encontrar buen feeling/ no lo he logrado

--- characters/cars/car_v1/car_v_1.gd
+++ characters/cars/car_v1/car_v_1.gd
@@ -12,13 +12,15 @@ extends VehicleBody3D
 @export var wheel_rear_right: VehicleWheel3D
 @export var suspension_travel := 0.05
 
+@export var camera: SpringArm3D
+
 
 var velocimetro_time := 0.5
 var _velocimetro_time = velocimetro_time
 
 # drifting
 @export var normal_grip := 8.0
-@export var drift_grip := 8.0
+@export var drift_grip := 1.5
 var grip
 
 var is_drifting := false
@@ -41,6 +43,7 @@ func _physics_process(delta: float) -> void:
     engine_force = dir * torque * (1.0 - RPM / max_RPM)
     steering = lerp(steering, steering_dir * turn_amount, turn_speed * delta)
 
+
     #no acelerando
     if dir == 0:
         brake = 3
@@ -48,20 +51,17 @@ func _physics_process(delta: float) -> void:
     #brake
     if Input.is_action_pressed('L2_button'):
         var brake_force = Input.get_action_strength('L2_button')
-        brake = lerp(brake, brake_force * 10, 0.1 * delta)
+        brake = lerp(brake, brake_force * 10, 1 * delta)
+        is_drifting = true
+    else:
+        is_drifting = false
         # print(brake)
 
     # Lateral velocity
     var right := global_transform.basis.x
     var lateral_speed: float = linear_velocity.dot(right)
 
-    # var grip = normal_grip
-
-    # if abs(lateral_speed) > 1.0:
-    if Input.is_action_pressed('a_button'):
-        is_drifting = true
-    else:
-        is_drifting = false
+    
 
     if is_drifting:
         grip = drift_grip
@@ -82,3 +82,7 @@ func _physics_process(delta: float) -> void:
     if _velocimetro_time < 0:
         _velocimetro_time = velocimetro_time
         print(engine_force)
+
+
+    #camera lerp
+    # camera.look_at(global_transform.basis.z,Vector3.UP)
