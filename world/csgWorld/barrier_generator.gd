4034383d4577c05b982ee7f84a507eb2dcf5cf2e
c-dez <c-dez@proton.me>
2026-09-09 22:12:17 -0600

    creado generar barreras / right

--- world/csgWorld/barrier_generator.gd
+++ world/csgWorld/barrier_generator.gd
@@ -10,19 +10,47 @@ extends Node3D
 
 @export var barrier_scene: PackedScene
 
+@export var generate: bool = false:
+    set(value):
+        generate = value
+        if value and Engine.is_editor_hint():
+            call_deferred('generate_barriers')
+            generate = false
+
+@export var clear: bool = false:
+    set(value):
+        clear = value
+        if value and Engine.is_editor_hint():
+            call_deferred('clear_barriers')
+            clear = false
+
+
+
 func _ready() -> void:
     if Engine.is_editor_hint():
         return
     
-    # generate_one()
-    generate()
+    generate_barriers()
+
+# func _process(_delta: float) -> void:
+#     if not Engine.is_editor_hint():
+#         return
+
+#     if generate:
+#         print("GENERATE DETECTADO")
+#         generate = false
+#         generate_barriers()
+
+#     if clear:
+#         clear = false
+#         clear_barriers()
 
 func clear_barriers() -> void:
     for child in %Right.get_children():
         child.queue_free()
 
 
-func generate() -> void: # generate_one()
+func generate_barriers() -> void: # generate_one()
     clear_barriers()
 
     if barrier_scene == null:
@@ -77,49 +105,3 @@ func generate() -> void: # generate_one()
         distance += spacing
         
 
-# func generate_one():
-#     if barrier_scene == null:
-#         return
-
-#     var path: Path3D = %Path3D
-#     var curve := path.curve
-
-#     if curve == null:
-#         return
-
-#     var length := curve.get_baked_length()
-
-#     if length <= 0.0:
-#         return
-
-#     var distance := 0.0
-
-#     var position := curve.sample_baked(distance, true)
-
-#     var next_position := curve.sample_baked(
-#         min(distance + 0.1, length),
-#         true
-#     )
-
-#     var direction := (
-#         next_position - position
-#     ).normalized()
-
-#     var side := Vector3.UP.cross(direction).normalized()
-
-#     position += side * offset
-#     position.y += height
-
-
-#     var barrier = barrier_scene.instantiate()
-
-#     $Right.add_child(barrier)
-
-#     barrier.global_position = path.to_global(position)
-
-#     var global_direction := path.to_global(position + direction) - path.to_global(position)
-
-#     barrier.look_at(
-#         barrier.global_position + global_direction,
-#         Vector3.UP
-#     )
